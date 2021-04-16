5e048ca0ad8110a1fda4421bf8128d565fc2005c
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-04-16 17:58:17 +0530

    test

--- src/Tours.js
+++ src/Tours.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Tour from './Tour';
 
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours}) => {
+  return <section>
+    <div className="title">
+      <h2>Tours</h2>
+      <div className='underline'></div>
+    </div>
+    <div>
+      {tours.map((tours) => {
+        return <Tour key={tours.id} {...tour}></Tour>
+      })}
+    </div>
+    </section>;
 };
 
 export default Tours;
